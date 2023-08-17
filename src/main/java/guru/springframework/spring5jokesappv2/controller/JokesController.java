40721217e64ca7c372f67bdd7274f196a7dd618c
Jovan-Stepanovic <Jovan.Stepanowic@nordstrom.com>
2023-08-17 14:10:55 +0200

    Created jokes controller and view, it displays new joke every time page refreshes. closes #3

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controller/JokesController.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring5jokesappv2.controller;
+
+import guru.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final JokeService jokeService;
+
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping("/")
+    public String getJoke(Model model) {
+        String chuckNorrisJoke = jokeService.getJoke();
+        model.addAttribute("joke", chuckNorrisJoke);
+        return "index";
+    }
+}
