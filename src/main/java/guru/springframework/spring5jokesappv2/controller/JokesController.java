6831a1f117d53c2591900d66f3b242fbe18544ee
Jovan-Stepanovic <Jovan.Stepanowic@nordstrom.com>
2023-09-03 15:07:10 +0200

    updated api adress

--- src/main/java/guru/springframework/spring5jokesappv2/controller/JokesController.java
+++ src/main/java/guru/springframework/spring5jokesappv2/controller/JokesController.java
@@ -14,7 +14,7 @@ public class JokesController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"/", ""})
+    @RequestMapping({"/", "/getJoke"})
     public String getJoke(Model model) {
         model.addAttribute("joke", jokeService.getJoke());
         return "index";
